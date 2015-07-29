d23718d2c2f0acc18ec117b78762fc5a7913fb7b
Clayton Turner <caturner3@g.cofc.edu>
2015-07-29 14:12:54 -0400

    Removing unnecessary code

--- code/pickle_data.py
+++ code/pickle_data.py
@@ -56,7 +56,6 @@ def pickle_data():
                              33, 34, 35, 36, 37, 38, 39,
                              40, 41, 42, 43, 44, 45, 46]
         encoder = OneHotEncoder(categorical_features=categorical_feats, sparse=False)
-        OneHotEncoder()
         data_mat = encoder.fit_transform(data_mat)
         y = data_mat[:,-1]
         x = data_mat[:,:-1]
