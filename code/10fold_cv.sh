97a23d59f30f139d45bc80ed5295dc3eb5e13231
Clayton Turner <caturner3@g.cofc.edu>
2015-08-04 14:20:52 -0400

    Pipeline using one results directory

--- code/10fold_cv.sh
+++ code/10fold_cv.sh
@@ -7,20 +7,36 @@ if [ $# -lt 2 ]
     exit
 fi
 
-res_loc1="../results/"
-res_loc2=$2
-res_loc3="_gpu/fold0"
-res_loc=$res_loc1$res_loc2$res_loc3
-out=".out"
+python pickle_data.py $1 1
+THEANO_FLAGS='cuda.root=/usr/local/cuda-7.0,floatX=float32,device=gpu0,nvcc.fastmath=True' python $2 1 $1 1 >> ../results/fold01.out
 
-for i in `seq 1 10`;
-do
-    python pickle_data.py $1 $i
-    if [ $i -lt 10 ]
-    then
-        res=$res_loc$i$out
-        python $2 1 $1 $i >> $res
-    else
-        python $2 1 $1 $i >> "../results/fold10.out"
-    fi
-done
+python pickle_data.py $1 2
+THEANO_FLAGS='cuda.root=/usr/local/cuda-7.0,floatX=float32,device=gpu0,nvcc.fastmath=True' python $2 1 $1 2 >> ../results/fold02.out
+
+python pickle_data.py $1 3
+THEANO_FLAGS='cuda.root=/usr/local/cuda-7.0,floatX=float32,device=gpu0,nvcc.fastmath=True' python $2 1 $1 3 >> ../results/fold03.out
+
+python pickle_data.py $1 4
+THEANO_FLAGS='cuda.root=/usr/local/cuda-7.0,floatX=float32,device=gpu0,nvcc.fastmath=True' python $2 1 $1 4 >> ../results/fold04.out
+
+python pickle_data.py $1 5
+THEANO_FLAGS='cuda.root=/usr/local/cuda-7.0,floatX=float32,device=gpu0,nvcc.fastmath=True' python $2 1 $1 5 >> ../results/fold05.out
+
+python pickle_data.py $1 6
+THEANO_FLAGS='cuda.root=/usr/local/cuda-7.0,floatX=float32,device=gpu0,nvcc.fastmath=True' python $2 1 $1 6 >> ../results/fold06.out
+
+python pickle_data.py $1 7
+THEANO_FLAGS='cuda.root=/usr/local/cuda-7.0,floatX=float32,device=gpu0,nvcc.fastmath=True' python $2 1 $1 7 >> ../results/fold07.out
+
+python pickle_data.py $1 8
+THEANO_FLAGS='cuda.root=/usr/local/cuda-7.0,floatX=float32,device=gpu0,nvcc.fastmath=True' python $2 1 $1 8 >> ../results/fold08.out
+
+python pickle_data.py $1 9
+THEANO_FLAGS='cuda.root=/usr/local/cuda-7.0,floatX=float32,device=gpu0,nvcc.fastmath=True' python $2 1 $1 9 >> ../results/fold09.out
+
+python pickle_data.py $1 10
+THEANO_FLAGS='cuda.root=/usr/local/cuda-7.0,floatX=float32,device=gpu0,nvcc.fastmath=True' python $2 1 $1 10 >> ../results/fold10.out
+
+cat ../results/01_p_values.txt ../results/02_p_values.txt ../results/03_p_values.txt ../results/04_p_values.txt ../results/05_p_values.txt ../results/06_p_values.txt ../results/07_p_values.txt ../results/08_p_values.txt ../results/09_p_values.txt ../results/10_p_values.txt > ../results/p_values.txt
+
+cat ../results/01_labels.txt ../results/02_labels.txt ../results/03_labels.txt ../results/04_labels.txt ../results/05_labels.txt ../results/06_labels.txt ../results/07_labels.txt ../results/08_labels.txt ../results/09_labels.txt ../results/10_labels.txt > ../results/labels.txt
